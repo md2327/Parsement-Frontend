d7a0166787a19e695d98392dc89f78e2f8c05fda
Mina Dang <md2327@users noreply.github.com>
2025-07-30 02:19:01 -0500

    Restored server package.json and updated server.js

--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@
 import express from "express"; // handles server
 import cors from "cors"; // handles cross-origin requests
 import path from "path"; // handles file paths
+import axios from "axios";
+import multer from "multer"; // parses file uploads in express
+import dotenv from "dotenv";
 import { fileURLToPath } from "url";
-
-const axios = require("axios");
-const multer = require("multer"); // parses file uploads in express
 
 // ===== handles frontend build ======
 const __filename = fileURLToPath(import.meta.url);
@@ -17,20 +17,14 @@ const __dirname = path.dirname(__filename);
 const app = express(); // initialize express
 
 app.use(cors()); // allow requests from CORS
-app.use(express.static(path.join(__dirname, "client"))); // for static frontend deployment to Render
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "index.html"));
-});
 
-const PORT = process.env.PORT || 8080; // Render's port
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // server starts listening
-
+// API routes comes before catch-all route
 const corsOption = { origin: ["http://localhost:5173"] };
 app.use(cors(corsOption));
 
 // ======= handles API fetching logic ========
+dotenv.config();
 const upload = multer({ storage: multer.memoryStorage() }); // stores file in memory
-require("dotenv").config();
 const apiKey = process.env.API_KEY; // securely stores api key in .env
 
 app.post("/parse", upload.single("file"), async (req, res) => {
@@ -54,5 +48,14 @@ app.post("/parse", upload.single("file"), async (req, res) => {
   } catch (error) {
     console.error("Error parsing resume:", error.message);
     res.status(500).json({ error: error.message });
-  }
+      }
+  });
+
+// static file serving and catch-all route for frontend
+app.use(express.static(path.join(__dirname, "client"))); // for static frontend deployment to Render
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
 });
+
+const PORT = process.env.PORT || 8080; // Render's port
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // server starts listening
